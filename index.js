162505009a99b91478ba4dea1ea314aac7f55703
lexy5251 <xixi5251@gmail.com>
2018-08-31 03:38:54 +0000

    get result

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  // let sum = 0;
+  // for (var i = 0; i < array.length; i++) {
+  //   sum = sum + array[i];
+  // }
+  // return sum;
+  let sum = 0;
+  for (item of array) {
+    if (Array.isArray(item)) {
+      sum += sumItems(item);
+
+    } else {
+      sum += item;
+    }
+
+  }
+
+  return sum;
+
+
 }
+
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
 
 module.exports = sumItems;
